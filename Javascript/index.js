f3d7bc099800ee6fbbffa38d404af33ef1cec6be
lydiamatz <lmatziaraki@gmail.com>
2025-06-29 19:05:36 +0300

    Added Multiple locations

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,31 +1,71 @@
-//los angeles time
+document.addEventListener("DOMContentLoaded", function () {
+  let selectedTimezone = null;
+  let intervalId = null;
 
-function updateTime() {
-  let losAngelesElement = document.querySelector("#los-angeles");
+  function updateDefaultClocks() {
+    // Los Angeles
+    let losAngelesElement = document.querySelector("#los-angeles");
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
 
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    // Paris
+    let parisElement = document.querySelector("#paris");
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
+    parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  function updateSelectedCityClock() {
+    if (!selectedTimezone) return;
+    let cityTime = moment().tz(selectedTimezone);
+    let dateElement = document.querySelector(".city.selected .date");
+    let timeElement = document.querySelector(".city.selected .time");
+    if (dateElement && timeElement) {
+      dateElement.innerHTML = cityTime.format("MMMM Do YYYY");
+      timeElement.innerHTML = cityTime.format("h:mm:ss [<small>]A[</small>]");
+    }
+  }
 
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+  function updateCity(event) {
+    selectedTimezone = event.target.value;
+    if (!selectedTimezone) return;
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    // Stop the default clocks interval
+    if (intervalId) clearInterval(intervalId);
 
-  //paris time
-  let parisElement = document.querySelector("#paris");
+    let cityTime = moment().tz(selectedTimezone);
+    let citiesElement = document.querySelector("#cities");
+    let cityName = selectedTimezone.split("/")[1].replace("_", " ");
+    let newCityHTML = `
+      <div class="city selected">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss [<small>]A[</small>]"
+        )}</div>
+      </div>
+    `;
+    citiesElement.innerHTML = newCityHTML;
 
-  let parisDateElement = parisElement.querySelector(".date");
+    // Start interval for selected city
+    intervalId = setInterval(updateSelectedCityClock, 1000);
+  }
 
-  let parisTimeElement = parisElement.querySelector(".time");
+  // On load, show LA and Paris clocks and update every second
+  updateDefaultClocks();
+  intervalId = setInterval(updateDefaultClocks, 1000);
 
-  let parisTime = moment().tz("Europe/Paris");
-
-  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
-}
-
-updateTime();
-setInterval(updateTime, 1000);
+  let citiesSelectElement = document.querySelector("#city");
+  citiesSelectElement.addEventListener("change", updateCity);
+});
